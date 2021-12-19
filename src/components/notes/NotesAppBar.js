f8f26c4fcdbf49220d95d9137a3780227f6aba0c
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2021-12-18 21:34:36 -0600

    adjusted the font size

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -4,10 +4,10 @@ export const NotesAppBar = () => {
     return (
         <div className='notes__appbar'>
             <span>20/June/2020</span>
-            <div>
+            
                 <button className='btn'>Picture</button>
                 <button className='btn'>Picture</button>
-            </div>
+            
         </div>
     )
 }
